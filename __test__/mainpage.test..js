74a157ec9b36b69ae785185bfb24345850ef98e3
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-27 17:57:11 -0700

    Files moved to proper folder, with correct names

--- __test__/mainpage.test..js
+++ __test__/mainpage.test..js
@@ -1,6 +1,3 @@
-const {describe, it} = intern.getPlugin('interface.bdd');
-const {  expect,  assert} = intern.getPlugin('chai');
-
 describe('Framework test', () => {
   it('Basic asssert', () => {
     assert.strictEqual(true, true, 'Yes, this is wrong');
